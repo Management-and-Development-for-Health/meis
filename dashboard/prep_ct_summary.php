6456e39f09828ea5d970c7e777959d227a312fb2
elinipendo <elinipendo143mziray@gmail.com>
2022-06-20 19:19:17 +0300

    pulling summarized data from filtered date is done

--- dashboard/prep_ct_summary.php
+++ dashboard/prep_ct_summary.php
@@ -3,33 +3,33 @@
                                                 <div class="card user-card2">
                                                     <div class="card-block text-center">
                                                         <h6 class="m-b-15">PrEP CT</h6>
-                                                        <h2 class="m-b-0 text text-success">5984</h2>
+                                                        <h2 class="m-b-0 text text-success"><?php echo htmlentities($total_count_prep_ct);?></h2>
                                                         <h6 class="m-b-10 m-t-10">Based on KVP groups</h6>
                                                         <a href="#!" class="text-c-blue b-b-primary">Export patient level data</a>
                                                         <div class="row justify-content-center m-t-10 b-t-default m-l-0 m-r-0">
                                                             <div class="col m-t-15 b-r-default">
                                                                 <h6 class="text-muted">FSW</h6>
-                                                                <h6>2455</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_fsw);?></h6>
                                                             </div>
                                                             <div class="col m-t-15 b-r-default">
                                                                 <h6 class="text-muted">MSM</h6>
-                                                                <h6>2455</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_msm);?></h6>
                                                             </div>
                                                             <div class="col m-t-15 b-r-default">
                                                                 <h6 class="text-muted">Discordant Couples</h6>
-                                                                <h6>2455</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_couples);?></h6>
                                                             </div>
                                                             <div class="col m-t-15 b-r-default">
                                                                 <h6 class="text-muted">AGYW</h6>
-                                                                <h6>2455</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_agyw);?></h6>
                                                             </div>
                                                             <div class="col m-t-15 b-r-default">
                                                                 <h6 class="text-muted">PWID</h6>
-                                                                <h6>2455</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_pwid);?></h6>
                                                             </div>
                                                             <div class="col m-t-15">
                                                                 <h6 class="text-muted">Risk Men</h6>
-                                                                <h6>300</h6>
+                                                                <h6><?php echo htmlentities($total_count_prep_ct_men);?></h6>
                                                             </div>
                                                         </div>
                                                     </div>
--- dashboard/total_prep_new_query_date.php
+++ dashboard/total_prep_new_query_date.php
@@ -4,11 +4,23 @@ require_once'../config.php';
 		$date1 = date("Y-m-d", strtotime($_POST['date1']));
 		$date2 = date("Y-m-d", strtotime($_POST['date2']));
 		$query=mysqli_query($link, "SELECT COUNT(uic)  from visits  where aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  uic");
-		$row=mysqli_num_rows($query);
-		if($row>0){
+		$query_fsw=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'FSW' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$query_msm=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'MSM' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$query_agyw=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'AGYW' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$query_pwid=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'PWID' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$query_couples=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'Discordant Couples' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$query_men=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'Risk Men' and aina_ya_hudhurio in ('mpya') and date(`tarehe_ya_hudhurio`) BETWEEN '$date1' AND '$date2' GROUP BY  visits.uic");
+		$GLOBALS['total_count_new_men']=mysqli_num_rows($query_men);
+		$GLOBALS['total_count_new_couples']=mysqli_num_rows($query_couples);
+		$GLOBALS['total_count_new_pwid']=mysqli_num_rows($query_pwid);
+		$GLOBALS['total_count_new_agyw']=mysqli_num_rows($query_agyw);
+		$GLOBALS['total_count_new_msm']=mysqli_num_rows($query_msm);
+		$GLOBALS['total_count_new_fsw']=mysqli_num_rows($query_fsw);
+		$GLOBALS['total_count_new']=mysqli_num_rows($query);
+		if($total_count_new>0){
 		
 ?>
- <h4 class="text-c-purple"><?php echo htmlentities($row);?></h4>
+ <h4 class="text-c-purple"><?php echo htmlentities($total_count_new);?></h4>
 <?php
 		
 		}else{
@@ -18,10 +30,28 @@ require_once'../config.php';
 	}else{
 		$query=mysqli_query($link, "select  COUNT(uic)  from visits  where aina_ya_hudhurio in ('mpya')and amepima_na_kupewa_majibu = 'hasi' and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
 		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE())  GROUP BY  uic");
-        $row=mysqli_num_rows($query);
+		$query_fsw=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'FSW' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$query_msm=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'MSM' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$query_agyw=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'AGYW' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$query_pwid=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'PWID' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$query_men=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'Risk Men' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$query_couples=mysqli_query($link, "SELECT COUNT(visits.uic), clients.kvp_group  from visits join clients on clients.uic =  visits.uic where clients.kvp_group = 'Discordant Couples' and aina_ya_hudhurio in ('mpya') and MONTH(tarehe_ya_hudhurio) = MONTH(CURRENT_DATE())
+		AND YEAR(tarehe_ya_hudhurio) = YEAR(CURRENT_DATE()) GROUP BY  visits.uic");
+		$GLOBALS['total_count_new_couples']=mysqli_num_rows($query_couples);
+		$GLOBALS['total_count_new_men']=mysqli_num_rows($query_men);
+		$GLOBALS['total_count_new_pwid']=mysqli_num_rows($query_pwid);
+		$GLOBALS['total_count_new_agyw']=mysqli_num_rows($query_agyw);
+		$GLOBALS['total_count_new_msm']=mysqli_num_rows($query_msm);
+		$GLOBALS['total_count_new_fsw']=mysqli_num_rows($query_fsw);
+        $GLOBALS['total_count_new']=mysqli_num_rows($query);
 		
 ?>
-	<h4 class="text-c-purple"><?php echo htmlentities($row);?></h4>
+	<h4 class="text-c-purple"><?php echo htmlentities($total_count_new);?></h4>
 <?php
 	
 	}
